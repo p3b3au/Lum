92f0070fc48f00886fac91a13ed18e7404b5c1bf
pebeau <pebeau@gmail.com>
2022-06-13 17:11:20 +0200

    temps restant pas encore!

---
+++ ajax.php
@@ -0,0 +1,16 @@
+
+<script src="https://code.jquery.com/jquery-3.5.1.js"> </script>
+<?php
+
+
+
+
+$.ajax ({
+    method: "POST",
+    url: "traitement.php"
+    data: { red:r, green: g}
+
+})
+
+
+
